b0ed1f2c49b9a2c954d8d48125ec28848d0ab82f
jhajagos <janos.hajagos@stonybrook.edu>
2024-02-01 12:54:00 -0500

    Added schema option

--- map/ohdsi/utilities/sqlserver/transfer_parquet_files_to_sql_server.py
+++ map/ohdsi/utilities/sqlserver/transfer_parquet_files_to_sql_server.py
@@ -3,7 +3,7 @@ import argparse
 import json
 
 
-def main(config, generated_tables_dict):
+def main(config, generated_tables_dict, schema=None):
 
     print(config)
     print(generated_tables_dict)
@@ -21,7 +21,10 @@ def main(config, generated_tables_dict):
             print(f"Reading: '{table}'")
             sdf = spark.read.parquet(parquet_path)
 
-            write_table_name = "transfer" + table.upper()
+            if schema is None:
+                write_table_name = "transfer" + table.upper()
+            else:
+                write_table_name = schema + "." + "transfer" + table.upper()
 
             print(f"Writing: '{write_table_name}'")
 
@@ -35,6 +38,7 @@ if __name__ == "__main__":
 
     arg_parser_obj.add_argument("-j", "--json-generated-tables", dest="json_generated_tables", default="../prepared_source/healtheintent/config.json.generated.parquet.json")
     arg_parser_obj.add_argument("-c", "--config-json-file-name", dest="config_json_file_name",  default="../prepared_source/healtheintent/config.json")
+    arg_parser_obj.add_argument("-s", "--schema", dest="schema_name", default=None)
     arg_parser_obj.add_argument("-l", "--run-local", dest="run_local", default=False, action="store_true")
 
     arg_obj = arg_parser_obj.parse_args()
@@ -60,4 +64,4 @@ if __name__ == "__main__":
     with open(arg_obj.config_json_file_name) as f:
         config = json.load(f)
 
-    main(config, generated_tables)
+    main(config, generated_tables, schema=arg_obj.schema_name)
