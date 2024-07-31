1fb22c865b52e94b50251a37dcc8bcc42eae31b8
jhajagos <janos.hajagos@stonybrook.edu>
2024-07-31 03:58:40 -0400

    Some initial code for parsing ccda documents

---
+++ map/prepared_source/cda/README.md
@@ -0,0 +1,6 @@
+## C-CDA to Prepared Source to OHDSI
+
+An individual will see multiple providers across different health systems. 
+Most patient portal now support export of medical records to the C-CDA format. 
+Code designed to take a collection of C-CDAs (Consolidated Clinical 
+Document Architecture)
---
+++ map/prepared_source/cda/cda_to_prepared_source.py
@@ -0,0 +1,92 @@
+import json
+import xml.etree.ElementTree as et
+
+# cda = et.parse(cda_filename)
+# observations = list(cda.iterfind("./{urn:hl7-org:v3}entry/{urn:hl7-org:v3}organizer/{urn:hl7-org:v3}component/{urn:hl7-org:v3}observation"))
+
+
+def extract_source_person_ccda(xml_doc):
+    # source_person
+    # ./ClinicalDocument/recordTarget/patientRole/patient/birthTime
+    # ./ClinicalDocument/recordTarget/patientRole/patient/administrativeGenderCode
+    # ./ClinicalDocument/recordTarget/patientRole/patient/raceCode
+    # ./ClinicalDocument/recordTarget/patientRole/patient/ethnicGroupCode
+    # ./ClinicalDocument/recordTarget/patientRole/patient/sdtc:deceasedInd
+    # ./ClinicalDocument/recordTarget/patientRole/patient/sdtc:deceasedTime
+
+    pass
+
+
+def extract_source_provider_ccda(xml_doc):
+    # source_provider
+    # ./ClinicalDocument/documentationOf/serviceEvent/performer
+    # ./ClinicalDocument/component/structuredBody/component/section/code --Get Section
+
+    pass
+
+
+def extract_problems_source_condition_ccda(xml_doc):
+    # Active problem lists
+    # /ClinicalDocument/component/structuredBody/component/section/entry/act/entryRelationship/observation/code[@code="404684003"][@codeSystem="2.16.840.1.113883.6.96"]/..
+    pass
+
+
+def extract_source_procedures_ccda(xml_doc):
+    # Procedures
+    # /ClinicalDocument/component/structuredBody/component/section/code[@code="47519-4"][@codeSystem="2.16.840.1.113883.6.1"]/../entry/observation
+    # /ClinicalDocument/component/structuredBody/component/section/code[@code="47519-4"][@codeSystem="2.16.840.1.113883.6.1"]/../entry/procedure
+    pass
+
+
+def extract_source_encounter_ccda(xml_doc):
+    # Encounters
+    # /ClinicalDocument/component/structuredBody/component/section/code[@code="46240-8"][@codeSystem="2.16.840.1.113883.6.1"]/../entry/encounter
+    pass
+
+
+def extract_source_medication_ccda(xml_doc):
+    # Medications
+    # /ClinicalDocument/component/structuredBody/component/section/code[@code="10160-0"][@codeSystem="2.16.840.1.113883.6.1"]/../entry/substanceAdministration
+    pass
+
+
+def extract_immunization_source_medication_ccda(xml_doc):
+    # Immunizations
+    # /ClinicalDocument/component/structuredBody/component/section/code[@code="11369-6"][@codeSystem="2.16.840.1.113883.6.1"]/../entry/substanceAdministration
+    pass
+
+
+def extract_labs_source_result_ccda(xml_doc):
+    # Labs
+    # /ClinicalDocument/component/structuredBody/component/section/code[@code="30954-2"][@codeSystem="2.16.840.1.113883.6.1"]/../entry/organizer/component/observation
+    pass
+
+
+def extract_vitals_source_result_ccda(xml_doc):
+    # Vitals
+    # ./ClinicalDocument/component/structuredBody/component/section/code[@code="8716-3"][@codeSystem="2.16.840.1.113883.6.1"]/../entry/organizer/component/observation
+    pass
+
+
+def extract_vitals_source_result_apple_cda(xml_doc):
+    # Vitals (Apple CDA)
+    # /ClinicalDocument/entry/organizer/code[@code="46680005"][@codeSystem="2.16.840.1.113883.6.96"]/../component/observation
+    pass
+
+
+def extract_social_history_source_condition(xml_doc):
+    # Social history
+    # /ClinicalDocument/component/structuredBody/component/section/code[@code="29762-2"][@codeSystem="2.16.840.1.113883.6.1"]/../entry/observation
+    pass
+
+
+def extract_source_note_ccda(xml_doc):
+    # Notes
+    # /ClinicalDocument/component/structuredBody/component/section/entry/act/code[@code="34109-9"][@codeSystem="2.16.840.1.113883.6.1"]/..
+    pass
+
+
+# Assessment and plan
+# /ClinicalDocument/component/structuredBody/component/section/code[@code="51847-2"][@codeSystem="2.16.840.1.113883.6.1"]/..
+
+
