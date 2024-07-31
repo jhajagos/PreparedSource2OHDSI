29ee3b57164dfd0418013b185a9f6ae1ccd513bf
jhajagos <janos.hajagos@stonybrook.edu>
2024-07-31 11:28:14 -0400

    Fixed case issue in truncate and wrote code for PSQL output

--- map/ohdsi/utilities/sqlserver/write_sql_server_insert_statements.py
+++ map/ohdsi/utilities/sqlserver/write_sql_server_insert_statements.py
@@ -111,7 +111,7 @@ def main(schema_dict, outfile_name, schema_name="dbo", transfer_table_prefix="tr
         if dialect == "mssql":
             sql_string += f"{en(sn)}.{en(transfer_table_prefix + tn)}"
         elif dialect == "psql":
-            sql_string += f"({en(sn)}.{en(transfer_table_prefix + tn.upper())}"
+            sql_string += f"{en(sn)}.{en(transfer_table_prefix + tn.upper())}"
 
 
         if table in custom_where:
