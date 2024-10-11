bd5093f754b46016864525eda8da268e519830d5
jhajagos <janos.hajagos@stonybrook.edu>
2024-10-11 16:09:12 -0400

    Added script to exclude loading concepts

--- map/prepared_source/synthea/docker/fully_load_ohdsi_parquet_files_excluding_concepts_into_rdbms.sh
+++ map/prepared_source/synthea/docker/fully_load_ohdsi_parquet_files_excluding_concepts_into_rdbms.sh
@@ -3,6 +3,6 @@
 WD=$(pwd)
 
 source ./stage_tables_excluding_concepts_into_rdbms.sh || exit
-source load_all_staged_tables_into_ohdsi_rdbms.sh || exit
+source ./load_staged_tables_into_ohdsi_rdbms.sh || exit
 
 cd $WD
