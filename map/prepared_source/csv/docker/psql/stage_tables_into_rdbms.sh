f14d8d9e7df7d1f8f512eb35592a0fd5ed6319f4
jhajagos <janos.hajagos@stonybrook.edu>
2024-10-16 00:05:47 -0400

    Made in sync with synthea container

---
+++ map/prepared_source/csv/docker/psql/stage_tables_into_rdbms.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+WD=$(pwd)
+cd /root/github/PreparedSource2OHDSI/map/ohdsi/utilities/sqlserver
+python transfer_parquet_files_to_sql_server.py -j /root/config/prepared_source_to_ohdsi_config.json.generated.parquet.json \
+ -c /root/config/prepared_source_to_ohdsi_config.json -s ohdsi -l
+
+cd $WD
