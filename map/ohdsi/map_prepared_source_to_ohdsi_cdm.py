e53ace9af9cfb7a12eac0a35ddfd084bce038eb9
jhajagos <janos.hajagos@stonybrook.edu>
2024-10-26 13:12:22 -0400

    Fix empty files not being written

--- map/ohdsi/map_prepared_source_to_ohdsi_cdm.py
+++ map/ohdsi/map_prepared_source_to_ohdsi_cdm.py
@@ -1424,7 +1424,13 @@ def write_source_to_parquet_partitioned_by_domain(source_matched_sdf, base_path,
     """Write out partitioned support table to parquet file partitioned by the mapped domain_id"""
     source_matched_path = base_path + "support/" + table_name +".parquet"
     logging.info(f"Writing '{table_name}' to '{source_matched_path}'")
-    source_matched_sdf.write.mode("overwrite").partitionBy("mapped_domain_id").parquet(source_matched_path)
+
+    if len(source_matched_sdf):
+        source_matched_sdf.write.mode("overwrite").partitionBy("mapped_domain_id").parquet(source_matched_path)
+    else:
+        source_matched_sdf.write.mode("overwrite").parquet(source_matched_path)
+        print(source_matched_sdf.printSchema())
+
     source_matched_sdf = spark.read.parquet(source_matched_path)
 
     return source_matched_sdf
