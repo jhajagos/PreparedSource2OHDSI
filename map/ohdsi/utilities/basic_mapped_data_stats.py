b7fa23932c03027fbdd2e4dac1cbf3fd0f5edf2a
jhajagos <janos.hajagos@stonybrook.edu>
2025-03-23 14:40:44 -0400

    Added custom spark configuration to other scripts

--- map/ohdsi/utilities/basic_mapped_data_stats.py
+++ map/ohdsi/utilities/basic_mapped_data_stats.py
@@ -3,6 +3,8 @@ import preparedsource2ohdsi.mapping_utilities as mu
 import pyspark
 import json
 import argparse
+import pprint
+from pyspark import SparkConf
 
 
 def main(spark, tbs):
@@ -69,33 +71,50 @@ if __name__ == "__main__":
     arg_parser_obj = argparse.ArgumentParser(description="Stage CSV files that confirm to the PreparedSource format for mapping to OHDSI")
     arg_parser_obj.add_argument("-c", "--config-json-file-name", dest="config_json_file_name", default="/root/config/prepared_source_to_ohdsi_config.json.generated.parquet.json")
     arg_parser_obj.add_argument("-l", "--run-local", dest="run_local", default=False, action="store_true")
+    arg_parser_obj.add_argument("--spark-config", dest="spark_config_file_name", default=None)
 
     arg_obj = arg_parser_obj.parse_args()
 
-    spark = pyspark.sql.SparkSession.builder \
-        .config("spark.driver.memory", "16g") \
-        .getOrCreate()
-
-    with open(arg_obj.config_json_file_name) as f:
-        tbs = json.load(f)
-
     RUN_LOCAL = arg_obj.run_local
 
-    if RUN_LOCAL:
-        spark = SparkSession.builder.appName("StageStreamlinedPreparedSource") \
-            .config('spark.driver.extraJavaOptions', '-Duser.timezone=GMT') \
-            .config('spark.executor.extraJavaOptions', '-Duser.timezone=GMT') \
-            .config('spark.sql.session.timeZone', 'UTC') \
-            .config("spark.driver.memory", "16g") \
-            .getOrCreate()
+    if arg_obj.spark_config_file_name is not None:
+        with open(arg_obj.spark_config_file_name, "r") as f:
+            extra_spark_configs = json.load(f)
     else:
-        spark = SparkSession.builder.appName("StageStreamlinedPreparedSource") \
-            .config('spark.driver.extraJavaOptions', '-Duser.timezone=GMT') \
-            .config('spark.executor.extraJavaOptions', '-Duser.timezone=GMT') \
-            .config('spark.sql.session.timeZone', 'UTC') \
-            .getOrCreate()
+        extra_spark_configs = {}
 
-    with open(arg_obj.config_json_file_name, mode="r") as f:
+    sconf = SparkConf()
+    default_spark_conf_dict = {
+        "spark.driver.extraJavaOptions": "-Duser.timezone=GMT",
+        "spark.executor.extraJavaOptions": "-Duser.timezone=GMT",
+        "spark.sql.session.timeZone": "UTC",
+    }
+
+    if RUN_LOCAL:
+        default_spark_conf_dict["spark.driver.memory"] =  "16g"
+    else:
+        pass
+
+    for key in extra_spark_configs:
+        if key in default_spark_conf_dict:
+            if key == "spark.driver.extraJavaOptions":
+                default_spark_conf_dict[key] += f" {extra_spark_configs[key]}"
+            elif key == "spark.executor.extraJavaOptions":
+                default_spark_conf_dict[key] += f" {extra_spark_configs[key]}"
+            else:
+                default_spark_conf_dict[key] = extra_spark_configs[key]
+        else:
+            default_spark_conf_dict[key] = extra_spark_configs[key]
+
+    print("Spark Configuration:")
+    pprint.pprint(default_spark_conf_dict)
+
+    for key in default_spark_conf_dict:
+        sconf.set(key, default_spark_conf_dict[key])
+
+    spark = SparkSession.builder.config(conf=sconf).appName("StageStreamlinedPreparedSource").getOrCreate()
+
+    with open(arg_obj.config_json_file_name) as f:
         config = json.load(f)
 
     main(spark, config)
--- map/ohdsi/utilities/sqlserver/transfer_parquet_files_to_sql_server.py
+++ map/ohdsi/utilities/sqlserver/transfer_parquet_files_to_sql_server.py
@@ -1,6 +1,8 @@
 from pyspark.sql import SparkSession
 import argparse
 import json
+import pprint
+from pyspark import SparkConf
 
 
 def main(config, generated_tables_dict, schema=None, exclude_concepts=False, exclude_prepared_source_tables=False):
@@ -47,30 +49,53 @@ if __name__ == "__main__":
 
     arg_parser_obj = argparse.ArgumentParser(description="Transfer staged OHDSI parquet files to staging tables in a SQL database")
 
-    arg_parser_obj.add_argument("-j", "--json-generated-tables", dest="json_generated_tables", default="../prepared_source/healtheintent/config.json.generated.parquet.json")
-    arg_parser_obj.add_argument("-c", "--config-json-file-name", dest="config_json_file_name",  default="../prepared_source/healtheintent/config.json")
+    arg_parser_obj.add_argument("-j", "--json-generated-tables", dest="json_generated_tables")
+    arg_parser_obj.add_argument("-c", "--config-json-file-name", dest="config_json_file_name")
     arg_parser_obj.add_argument("-s", "--schema", dest="schema_name", default=None)
     arg_parser_obj.add_argument("-l", "--run-local", dest="run_local", default=False, action="store_true")
     arg_parser_obj.add_argument("-x", "--exclude-concept-tables", dest="exclude_concept_tables", default=False, action="store_true")
     arg_parser_obj.add_argument("--exclude-prepared-source-tables", dest="exclude_prepared_source_tables", default=False, action="store_true")
-
+    arg_parser_obj.add_argument("--spark-config", dest="spark_config_file_name", default=None)
 
     arg_obj = arg_parser_obj.parse_args()
     RUN_LOCAL = arg_obj.run_local
 
-    if RUN_LOCAL:
-        spark = SparkSession.builder.appName("ToStagingDBTable") \
-            .config('spark.driver.extraJavaOptions', '-Duser.timezone=GMT') \
-            .config('spark.executor.extraJavaOptions', '-Duser.timezone=GMT') \
-            .config('spark.sql.session.timeZone', 'UTC') \
-            .config("spark.driver.memory", "16g") \
-            .getOrCreate()
+    if arg_obj.spark_config_file_name is not None:
+        with open(arg_obj.spark_config_file_name, "r") as f:
+            extra_spark_configs = json.load(f)
     else:
-        spark = SparkSession.builder.appName("ToStagingDBTable") \
-            .config('spark.driver.extraJavaOptions', '-Duser.timezone=GMT') \
-            .config('spark.executor.extraJavaOptions', '-Duser.timezone=GMT') \
-            .config('spark.sql.session.timeZone', 'UTC') \
-            .getOrCreate()
+        extra_spark_configs = {}
+
+    sconf = SparkConf()
+    default_spark_conf_dict = {
+        "spark.driver.extraJavaOptions": "-Duser.timezone=GMT",
+        "spark.executor.extraJavaOptions": "-Duser.timezone=GMT",
+        "spark.sql.session.timeZone": "UTC",
+    }
+
+    if RUN_LOCAL:
+        default_spark_conf_dict["spark.driver.memory"] =  "16g"
+    else:
+        pass
+
+    for key in extra_spark_configs:
+        if key in default_spark_conf_dict:
+            if key == "spark.driver.extraJavaOptions":
+                default_spark_conf_dict[key] += f" {extra_spark_configs[key]}"
+            elif key == "spark.executor.extraJavaOptions":
+                default_spark_conf_dict[key] += f" {extra_spark_configs[key]}"
+            else:
+                default_spark_conf_dict[key] = extra_spark_configs[key]
+        else:
+            default_spark_conf_dict[key] = extra_spark_configs[key]
+
+    print("Spark Configuration:")
+    pprint.pprint(default_spark_conf_dict)
+
+    for key in default_spark_conf_dict:
+        sconf.set(key, default_spark_conf_dict[key])
+
+    spark = SparkSession.builder.config(conf=sconf).appName("StageStreamlinedPreparedSource").getOrCreate()
 
     with open(arg_obj.json_generated_tables, "r") as f:
         generated_tables = json.load(f)
