228350ca8f402f9401646cb7427025671ca219ea
jhajagos <janos.hajagos@stonybrook.edu>
2026-02-26 11:33:41 -0500

    added g_source_system for mortality records

--- map/ohdsi/utilities/basic_mapped_data_stats.py
+++ map/ohdsi/utilities/basic_mapped_data_stats.py
@@ -48,6 +48,8 @@ def main(spark, tbs, extended_queries, output_csv_files, output_base_directory):
 
                           "drug_concepts_count": "select count(distinct person_id) as n, count(*) as n_r, drug_concept_id, c.concept_name from drug_exposure de join concept c on c.concept_id = de.drug_concept_id group by drug_concept_id, c.concept_name order by count(*) desc",
 
+                          "source_drug_concepts_count": "select count(*) as n_r, count(distinct person_id) as n, drug_source_concept_id, c1.concept_name, c1.concept_code, c1.vocabulary_id,  c1.concept_class_id from drug_exposure de join concept c1 on de.drug_source_concept_id = c1.concept_id group by drug_source_concept_id, c1.concept_name, c1.concept_code, c1.vocabulary_id, c1.concept_class_id order by count(*) desc",
+
                           "measurement_concepts_count": "select count(distinct person_id) as n, count(*) as n_r, measurement_concept_id, c.concept_name as measurement_concept_name, "
                                                         "min(value_as_number) as min_value_as_number, max(value_as_number) as max_value_as_number, c2.concept_code as unit_concept_code, "
                                                         " min(measurement_date) as min_measurement_date, max(measurement_date) as max_measurement_date, percentile(value_as_number, 0.05) as p05, percentile(value_as_number, 0.25) as p25, "
@@ -89,7 +91,8 @@ def main(spark, tbs, extended_queries, output_csv_files, output_base_directory):
         "yearly_device_counts": "select count(distinct person_id) as n, count(distinct visit_occurrence_id) as n_visits, count(1) as n_r, device_year from (select  person_id, visit_occurrence_id, extract(year from device_exposure_start_date) as device_year from device_exposure) t group by device_year order by device_year desc",
         "measurement_units_ranges": "select t.*, 1 - n_r_no_units / n_r, 1 - n_r_no_high_low / n_r from (select count(*) as n_r, count(distinct m.person_id), c1.concept_name, c1.concept_id, sum(case when unit_concept_id is null or unit_concept_id = 0 then 1 else 0 end) as n_r_no_units, sum(case when range_high is null and range_low is null then 1 else 0 end) as n_r_no_high_low from measurement m join concept c1 on c1.concept_id = m.measurement_concept_id group by c1.concept_id, c1.concept_name) t order by n_r desc",
         "measurement_units_ranges_by_source": "select t.*, 1 - n_r_no_units / n_r, 1 - n_r_no_high_low / n_r from (select count(*) as n_r, count(distinct m.person_id), c1.concept_name, c1.concept_id, g_source_system, sum(case when unit_concept_id is null or unit_concept_id = 0 then 1 else 0 end) as n_r_no_units, sum(case when range_high is null and range_low is null then 1 else 0 end) as n_r_no_high_low from measurement m join concept c1 on c1.concept_id = m.measurement_concept_id group by c1.concept_id, c1.concept_name, g_source_system) t order by n_r desc",
-        "drugs_not_mapped_to_standard_concepts": "select t.*, c1.concept_code, c1.vocabulary_id, c1.concept_class_id, c1.concept_name from (select drug_source_value, drug_source_concept_id, count(*) as n_r, count(distinct person_id) as n from drug_exposure where drug_concept_id = 0 group by drug_source_value,drug_source_concept_id) t join concept c1 on c1.concept_id = t.drug_source_concept_id order by n_r desc"
+        "drugs_not_mapped_to_standard_concepts": "select t.*, c1.concept_code, c1.vocabulary_id, c1.concept_class_id, c1.concept_name from (select drug_source_value, drug_source_concept_id, count(*) as n_r, count(distinct person_id) as n from drug_exposure where drug_concept_id = 0 group by drug_source_value,drug_source_concept_id) t join concept c1 on c1.concept_id = t.drug_source_concept_id order by n_r desc",
+        "drug_not_mapped_to_concept_ids": "select drug_source_value, count(*) as n_r, count(distinct person_id) as n from drug_exposure de where drug_source_concept_id = 0 group by drug_source_value order by count(*) desc"
     }
 
     if extended_queries is True:
@@ -120,6 +123,10 @@ def main(spark, tbs, extended_queries, output_csv_files, output_base_directory):
         q_columns = q_df.columns
         if "p_n" in q_columns and "n" in q_columns:
             q_df["n / p_n"] = q_df["n"] / q_df["p_n"]
+
+        if "n_r" in q_columns: # For total calculate fractions of records
+            sum_n_r = q_df["n_r"].sum()
+            q_df["n_r / sum_n_r"] = q_df["n_r"] / sum_n_r
 
         print(q_df)
         print("")
